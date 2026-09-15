8da40e8821948f11a71d6aed954fccfd1199d867
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2026-09-15 13:16:55 +0200

    refacto: drop dead code and unused files

--- src/components/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export default function Logo() {
-  return <div className="absolute left-8 top-8 font-bold text-2xl">Vexia</div>;
-}
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+// Entry point for shared constants and helpers
+export { templates } from "./constants/templates.constants";
+export { FAKE_PREVIEW } from "./constants/fakePreview.constants";
+export { capitalize } from "./helpers/capitalize.helpers";
