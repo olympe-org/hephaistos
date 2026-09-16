157cdd9c8341267fdf497ceba0cc5008846170ff
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2026-09-16 11:12:57 +0200

    fix: PWA icon

--- vite.config.ts
+++ vite.config.ts
@@ -42,12 +42,6 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "/icons/icon-512-maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
         ],
       },
     }),
